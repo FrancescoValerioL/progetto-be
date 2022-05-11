8542e285b17cc8f50078f9f76b3d886181fd14c8
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-11 18:11:35 +0200

    LandingPage + Navbar

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,8 +6,8 @@ const LandingPage = () => {
     return(
         <Container fluid>
             <MyNavbar />
-            <Row>
-                <Col className="d-flex justify-content-center">
+            <Row className="h-100">
+                <Col className="d-flex align-items-center justify-content-center" >
                     <h1>Contenuto</h1>
                 </Col>
             </Row>
--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { MDBBtn, MDBBtnGroup } from 'mdb-react-ui-kit';
+
 import {
   MDBNavbar,
   MDBContainer,
@@ -8,45 +10,48 @@ import {
   MDBNavbarItem,
   MDBNavbarLink,
   MDBNavbarToggler,
-  MDBCollapse
-} from 'mdb-react-ui-kit';
+  MDBCollapse,
+} from "mdb-react-ui-kit";
 
 const MyNavbar = () => {
   const [showNavText, setShowNavText] = useState(false);
 
   return (
-    <MDBNavbar expand='lg' light bgColor='light'>
+    <MDBNavbar expand="lg" light bgColor="light">
       <MDBContainer fluid>
-        <MDBNavbarBrand href='#'>Progetto Be</MDBNavbarBrand>
+        <MDBNavbarBrand href="#">Progetto Be</MDBNavbarBrand>
         <MDBNavbarToggler
-          type='button'
-          data-target='#navbarText'
-          aria-controls='navbarText'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          type="button"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
           onClick={() => setShowNavText(!showNavText)}
         >
-          <MDBIcon icon='bars' fas />
+          <MDBIcon icon="bars" fas />
         </MDBNavbarToggler>
         <MDBCollapse navbar show={showNavText}>
-          <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+          <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
             <MDBNavbarItem>
-              <MDBNavbarLink active aria-current='page' href='#'>
+              <MDBNavbarLink active aria-current="page" href="#">
                 Home
               </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Film</MDBNavbarLink>
+              <MDBNavbarLink href="#">Film</MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Serie</MDBNavbarLink>
+              <MDBNavbarLink href="#">Serie</MDBNavbarLink>
             </MDBNavbarItem>
           </MDBNavbarNav>
-          <span className='navbar-text'> Accedi / Registrati </span>
+          
+            <MDBBtn color="success"  className='mx-2 text-nowrap'>Login</MDBBtn>
+            <MDBBtn  className='mx-2 text-nowrap'>Register</MDBBtn>
+          
         </MDBCollapse>
       </MDBContainer>
     </MDBNavbar>
   );
-}
+};
 
-export default MyNavbar
+export default MyNavbar;
