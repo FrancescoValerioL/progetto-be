831a8d00754d5d69d020663b6b8c5a09da22a643
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-11 17:04:51 +0200

    Sistemato cose varie

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import BootsrapComponent from './components/BootstrapComponent';
 import MDBComponent from './components/MDBComponent';
 import AntComponent from './components/AntComponent';
+import MyNavbar from './components/MyNavbar';
 
 function App() {
   return (
     <>
+    <MyNavbar />
     <BootsrapComponent />
     <AntComponent />
     <MDBComponent />
---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarBrand,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBCollapse
+} from 'mdb-react-ui-kit';
+
+const MyNavbar = () => {
+  const [showNavText, setShowNavText] = useState(false);
+
+  return (
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>Progetto Be</MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          data-target='#navbarText'
+          aria-controls='navbarText'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavText(!showNavText)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar show={showNavText}>
+          <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Accedi</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Registrati</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+          <span className='navbar-text'> Navbar text with an inline element </span>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
+
+export default MyNavbar
