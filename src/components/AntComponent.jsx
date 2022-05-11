6a788763c61d0eabc9017f022b1f52602b5f38eb
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-11 13:05:22 +0200

    creati primi file di prova

---
+++ src/components/AntComponent.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { AutoComplete } from 'antd';
+import { Button } from 'react-bootstrap';
+
+const mockVal = (str, repeat = 1) => ({
+  value: str.repeat(repeat),
+});
+
+const AntComponent = () => {
+  const [value, setValue] = useState('');
+  const [options, setOptions] = useState([]);
+
+  const onSearch = (searchText) => {
+    setOptions(
+      !searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)],
+    );
+  };
+
+  const onSelect = (data) => {
+    console.log('onSelect', data);
+  };
+
+  const onChange = (data) => {
+    setValue(data);
+  };
+
+  return (
+    <>
+      <AutoComplete
+        options={options}
+        style={{
+          width: 200,
+        }}
+        onSelect={onSelect}
+        onSearch={onSearch}
+        placeholder="input here"
+      />
+      <br />
+      <Button variant='outline-primary'>Bottone</Button>
+      <br />
+      <AutoComplete
+        value={value}
+        options={options}
+        style={{
+          width: 200,
+        }}
+        onSelect={onSelect}
+        onSearch={onSearch}
+        onChange={onChange}
+        placeholder="control mode"
+      />
+    </>
+  );
+};
+
+export default AntComponent;
