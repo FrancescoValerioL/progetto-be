073ea253996309beeb3a3076bc6c9a865f2d683e
Alberto02001 <albertosoldiviero@email.com>
2022-05-12 11:22:01 +0200

    footer aggiornato

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -13,7 +13,7 @@ const MyFooter = () => {
       <MDBContainer className='pt-4'>
         <section className='mb-4'>
           <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
+            className='btn btn-link btn-lg text-dark m-1'
             href='#!'
             role='button'
             data-mdb-ripple-color='dark'
@@ -22,7 +22,7 @@ const MyFooter = () => {
           </a>
 
           <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
+            className='btn btn-link btn-lg text-dark m-1'
             href='#!'
             role='button'
             data-mdb-ripple-color='dark'
@@ -31,7 +31,7 @@ const MyFooter = () => {
           </a>
 
           <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
+            className='btn btn-link btn-lg text-dark m-1'
             href='#!'
             role='button'
             data-mdb-ripple-color='dark'
@@ -40,7 +40,7 @@ const MyFooter = () => {
           </a>
 
           <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
+            className='btn btn-link btn-lg text-dark m-1'
             href='#!'
             role='button'
             data-mdb-ripple-color='dark'
@@ -49,7 +49,7 @@ const MyFooter = () => {
           </a>
 
           <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
+            className='btn btn-link btn-lg text-dark m-1'
             href='#!'
             role='button'
             data-mdb-ripple-color='dark'
@@ -58,7 +58,7 @@ const MyFooter = () => {
           </a>
 
           <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
+            className='btn btn-link btn-lg text-dark m-1'
             href='#!'
             role='button'
             data-mdb-ripple-color='dark'
