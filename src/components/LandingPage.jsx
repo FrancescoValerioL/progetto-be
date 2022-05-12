a1e0e9dd56be83f4b581d98ee078e35482444a71
Alberto02001 <albertosoldiviero@email.com>
2022-05-12 14:22:13 +0200

    carosello

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import CaroselloHome from "./caroselloHome";
 import MyFooter from "./MyFooter";
 import MyNavbar from "./MyNavbar";
-
 
 const LandingPage = () => {
     return(
@@ -14,6 +14,11 @@ const LandingPage = () => {
             <Row className="h-100">
                 <Col className="d-flex align-items-center justify-content-center" >
                     <h1>Contenuto</h1>
+                </Col>
+            </Row>
+            <Row className="d-flex align-items-center justify-content-center">
+                <Col md={3}>
+                <CaroselloHome titolo='Film-1' img='https://image.tmdb.org/t/p/original/7MDgiFOPUCeG74nQsMKJuzTJrtc.jpg'/>
                 </Col>
             </Row>
             <MyFooter />
---
+++ src/components/caroselloHome.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  MDBCarousel,
+  MDBCarouselInner,
+  MDBCarouselItem,
+  MDBCarouselElement,
+  MDBCarouselCaption,
+} from 'mdb-react-ui-kit';
+
+const CaroselloHome = (props) => {
+  return (
+    <MDBCarousel showIndicators showControls fade>
+      <MDBCarouselInner>
+        <MDBCarouselItem className='active'>
+          <MDBCarouselElement src={props.img} alt={props.titolo} />
+          <MDBCarouselCaption>
+            <h5>{props.titolo}</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+
+        <MDBCarouselItem>
+          <MDBCarouselElement src={props.img} alt={props.titolo} />
+          <MDBCarouselCaption>
+            <h5>{props.titolo}</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+
+        <MDBCarouselItem>
+          <MDBCarouselElement src={props.img} alt={props.titolo} />
+          <MDBCarouselCaption>
+            <h5>{props.titolo}</h5>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+      </MDBCarouselInner>
+    </MDBCarousel>
+  );
+}
+
+
+export default CaroselloHome
