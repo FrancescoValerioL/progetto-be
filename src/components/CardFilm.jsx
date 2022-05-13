59e96f0a5541a76cc3e40f59e7142702be872aeb
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-13 14:36:09 +0200

    Card e fetch singolo film

---
+++ src/components/CardFilm.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBCardFooter,
+} from "mdb-react-ui-kit";
+
+const CardFilm = (props) => {
+  const mappaturaGenres = () => {
+    console.log("Sono nella funzione");
+    if (props.genres != undefined) {
+      const myVet = [...props.genres];
+
+      let dato = "";
+      for (let i = 0; i < myVet.length; i++) {
+        dato += myVet[i].name + " ";
+      }
+      console.log("============================");
+      console.log(myVet);
+      return "Generi: " + dato;
+    }
+  };
+
+  return (
+    <MDBCard style={{ maxWidth: "22rem" }}>
+      <MDBCardImage src={props.img} position="top" alt={props.title} />
+      <MDBCardBody>
+        <MDBCardTitle>{props.title}</MDBCardTitle>
+        <MDBCardText>
+          {props.original_title}
+
+          {props.overview}
+        </MDBCardText>
+      </MDBCardBody>
+      <MDBCardFooter>{mappaturaGenres()}</MDBCardFooter>
+    </MDBCard>
+  );
+};
+export default CardFilm;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,49 @@
-
+import { useState, useEffect } from "react";
 import { MDBBtn } from "mdb-react-ui-kit";
 import { Navbar, Container, Col, Button, Row } from "react-bootstrap";
+import CardFilm from "../components/CardFilm";
+import axios from "axios";
 
+const Home = () => {
+  const [film, setFilm] = useState({});
 
+  useEffect(() => {
+    fetchFilm();
+  }, []);
 
-const Home = () =>{
+  const fetchFilm = (id) => {
+    axios
+      .get(`http://localhost:2000/api/datiFilm?idFilm=${id}`)
+      .then(function (response) {
+        setFilm(response.data);
+      });
+  };
 
-   
-    return(
-        <>
-        <Container>
-            <Row className="mt-5 d-flex align-items-center justify-content-center">
-                <Col md={8} className="d-flex justify-content-center mt-2">
-                <h1>Home</h1>
-                <MDBBtn href="/">Landing</MDBBtn>
-                </Col>
-            </Row>
-        </Container>
-        
-        </>
-    )
-}
+  return (
+    <>
+      <Container>
+        <Row className="mt-5 d-flex align-items-center justify-content-center">
+          <Col md={8} className="d-flex justify-content-center mt-2">
+            {film.length != 0 ? (
+              <CardFilm
+                title={film.title}
+                original_title={film.original_title}
+                img={film.img}
+                overview={film.overview}
+                genres={film.genres}
+              />
+            ) : (
+              <></>
+            )}
 
+          </Col>
+          <Col>
+          <MDBBtn href="/">Landing</MDBBtn>  
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-export default Home
+export default Home;
