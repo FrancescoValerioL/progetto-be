e212bf09302570f036cd08e33539e45fa77ba9c4
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-15 16:41:44 +0200

    Revert "slider home"
    This reverts commit 38ead3ee99296468cdc7430ebbaf2ff6f4990292.

--- src/components/esempioOverlay.jsx
+++ src/components/esempioOverlay.jsx
@@ -1,6 +1,6 @@
 import { MDBBtn } from "mdb-react-ui-kit";
 import { useState } from "react";
-import { Card, Modal, Row } from "react-bootstrap";
+import { Card, Modal, Button } from "react-bootstrap";
 
 const GuardaFilm = (props) => {
   const [show, setShow] = useState(false);
@@ -10,27 +10,18 @@ const GuardaFilm = (props) => {
 
   return (
     <>
-      <MDBBtn onClick={handleShow} className="myButton">
-        Guarda
-      </MDBBtn>
+    <MDBBtn onClick={handleShow} className="myButton">Guarda</MDBBtn>
       <Modal size="xl" show={show} onHide={handleClose}>
-        <Modal.Header style={{backgroundColor: '#658ffb', color: 'white'}} closeButton>
+        <Modal.Header closeButton>
           <Modal.Title>{props.nome}</Modal.Title>
         </Modal.Header>
-        <Modal.Body style={{backgroundColor: '#b3d6ff'}}>
-          <Row className="mt-3 d-flex align-items-center justify-content-center">
-          <iframe allowfullscreen="allowfullscreen" height="400" style={{ width: "90%" }} src={props.video}></iframe>
-          </Row>
-          <Row className="mt-5 d-flex align-items-center justify-content-center">
-          <p className="textPlayVideo">{props.text}</p>
-          </Row>
+        <Modal.Body>
+          <img src={props.img} style={{ width: "100%" }} />
         </Modal.Body>
+        <Modal.Footer></Modal.Footer>
       </Modal>
     </>
   );
 };
 
 export default GuardaFilm;
-
-//<img src={props.img} style={{ width: "100%" }} />
-
