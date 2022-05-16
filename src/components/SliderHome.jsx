ebde7c714cb4ccfa6563b58647a66d57c1e4009f
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-16 11:13:23 +0200

    backdrop_path aggiunto

--- src/components/SliderHome.jsx
+++ src/components/SliderHome.jsx
@@ -4,56 +4,51 @@ import GuardaFilm from "./esempioOverlay";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-
 const SliderHome = () => {
   let [films, setFilms] = useState([]);
 
   useEffect(() => {
-    films = []
+    films = [];
     axios
-    .get(`http://localhost:2000/api/movie/topRated`)
-    .then(function (response) {
-      console.log(response.data)
-      setFilms(response.data)
-      console.log(films)
-      
-    });
-  },[])
+      .get(`http://localhost:2000/api/movie/topRated`)
+      .then(function (response) {
+        console.log(response.data);
+        setFilms(response.data);
+        console.log(films);
+      });
+  }, []);
 
-  const createCarousel = () =>{
-    for(let i = 0; i < films.length / 5; i++){
+  const createCarousel = () => {
+    for (let i = 0; i < films.length / 5; i++) {
       //da implementare
     }
-  }
+  };
 
-
-  return (
-    films.length != 0 ?
+  return films.length != 0 ? (
     <>
-    <Container className="mt-3 d-flex align-items-center justify-content-center">
-      <Carousel fade className="d-block w-75">
-        
-        {films.map((el) => (
-          <Carousel.Item key={el.id} interval={1000}>
-            <div className="drk side-crop">
-              <img className="d-block w-100" src={el.img} alt="First slide" />
-            </div>
-            <Carousel.Caption className="mb-5">
-              <h5 className="titoloSlider">{el.title}</h5>
-              <GuardaFilm nome={el.title} video={el.img} text={el.title}/>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-        
-      </Carousel>
+      <Container className="mt-3 d-flex align-items-center justify-content-center">
+        <Carousel fade className="d-block w-75">
+          {films.map((el) => (
+            <Carousel.Item key={el.id} interval={1000}>
+              <div className="drk side-crop">
+                <img
+                  className="d-block w-100"
+                  src={el.backdrop_img}
+                  alt="First slide"
+                />
+              </div>
+              <Carousel.Caption className="mb-5">
+                <h5 className="titoloSlider">{el.title}</h5>
+                <GuardaFilm nome={el.title} video={el.img} text={el.title} />
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
       </Container>
-    </>:
+    </>
+  ) : (
     <></>
   );
 };
 
 export default SliderHome;
-
-
-
-
