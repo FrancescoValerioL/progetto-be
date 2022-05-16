d5a40c3f040f0f64ce6922c715d781d574016ba4
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-16 10:23:49 +0200

    funzioni da file esterno

--- src/pages/Horizontal.jsx
+++ src/pages/Horizontal.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavbarHome from "../components/NavbarHome";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import { topRated } from "../scripts/movie";
 import axios from "axios";
 
 const Horizontal = () => {
@@ -9,27 +10,26 @@ const Horizontal = () => {
   let [filmsTrending, setTrending] = useState([]);
   let [trendingTv, setTrendingTv] = useState([]);
 
+  async function asyncCall() {
+    console.log("calling");
+    const result = await topRated();
+    setFilms(result);
+  }
+
   useEffect(() => {
-    axios
-      .get(`http://localhost:2000/api/movie/topRated`)
-      .then(function (response) {
-        console.log(response.data);
-        setFilms(response.data);
-        console.log(filmsTop);
-      });
+    asyncCall();
+  }, []);
+
+  useEffect(() => {
     axios
       .get(`http://localhost:2000/api/movie/trendingWeek`)
       .then(function (response) {
-        console.log(response.data);
         setTrending(response.data);
-        console.log(filmsTop);
       });
     axios
       .get(`http://localhost:2000/api/tv/trendingWeek`)
       .then(function (response) {
-        console.log(response.data);
         setTrendingTv(response.data);
-        console.log(filmsTop);
       });
   }, []);
 
@@ -39,11 +39,15 @@ const Horizontal = () => {
       <Container fluid className="overflow-auto bg-row-1 horizontalrow">
         <h2>Top Movie</h2>
         <Row className="flex-nowrap mt-2 mb-2">
-          {filmsTop.map((element) => (
-            <Col key={element.id}>
-              <Image src={element.img} style={{ width: "300px" }} />{" "}
-            </Col>
-          ))}
+          {filmsTop != undefined ? (
+            filmsTop.map((element) => (
+              <Col key={element.id}>
+                <Image src={element.img} style={{ width: "300px" }} />{" "}
+              </Col>
+            ))
+          ) : (
+            <></>
+          )}
         </Row>
       </Container>
       <Container fluid className="overflow-auto bg-row-2 horizontalrow">
--- src/scripts/movie.js
+++ src/scripts/movie.js
@@ -1,24 +1,29 @@
 import axios from "axios";
 
 const trendingWeek = () => {
-    let films = []
-    axios
+  let films = [];
+  axios
     .get(`http://localhost:2000/api/movie/trendingWeek`)
     .then(function (response) {
-      films = response.data.map(elem => elem);
-      console.log(films)
+      films = response.data.map((elem) => elem);
+      console.log(films);
     });
-}
-const topRated = () => {
-    let films = []
+};
+function topRated() {
+  return new Promise((resolve) => {
+    let films = [];
     axios
-    .get(`http://localhost:2000/api/movie/topRated`)
-    .then(function (response) {
-      films = response.data.map(elem => elem);
-      console.log(films)
-      return films
-    });
-    
+      .get(`http://localhost:2000/api/movie/topRated`)
+      .then(function (response) {
+        films = response.data.map((elem) => elem);
+
+        resolve(films);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  });
 }
 
-export { trendingWeek, topRated }
+export { trendingWeek, topRated };
