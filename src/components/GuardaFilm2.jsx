ad22364c60fafa405ebb2b6089821798f3ba9c1a
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-17 15:24:41 +0200

    Trailer di tutte cose in home2

--- src/components/GuardaFilm2.jsx
+++ src/components/GuardaFilm2.jsx
@@ -1,28 +1,59 @@
 import { MDBBtn } from "mdb-react-ui-kit";
 import { useState } from "react";
 import { Card, Modal, Row, Image } from "react-bootstrap";
+import { selectVideo } from "../scripts/selectVideo";
 
 const GuardaFilm2 = (props) => {
   const [show, setShow] = useState(false);
+  const [trailer, setTrailer] = useState("");
+  const [id, setId] = useState("");
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  async function asyncCall(movie_id) {
+    console.log("calling selectVideo");
+    console.log(movie_id);
+    const result = await selectVideo(movie_id);
+    setTrailer(result);
+    console.log(movie_id);
+    console.log("risultato");
+    console.log(result);
+  }
+
+  const handleButtonClick = (e) => {
+    handleShow();
+    console.log(e.target.offsetParent.value);
+    asyncCall(e.target.offsetParent.value);
+  };
+
   return (
     <>
-    <a className="imgRigaHover" onClick={handleShow}>
-      <Image className="imgRiga" src={props.img}/>
-      </a>
+      <MDBBtn
+        className="imgRigaHover bg-powder-blue"
+        onClick={(e) => handleButtonClick(e)}
+        value={props.id}
+      >
+        <Image className="imgRiga" src={props.img} value={props.id} />
+      </MDBBtn>
       <Modal size="xl" show={show} onHide={handleClose}>
-        <Modal.Header style={{backgroundColor: '#658ffb', color: 'white'}} closeButton>
+        <Modal.Header
+          style={{ backgroundColor: "#658ffb", color: "white" }}
+          closeButton
+        >
           <Modal.Title>{props.nome}</Modal.Title>
         </Modal.Header>
-        <Modal.Body style={{backgroundColor: '#b3d6ff'}}>
+        <Modal.Body style={{ backgroundColor: "#b3d6ff" }}>
           <Row className="mt-3 d-flex align-items-center justify-content-center">
-          <iframe allowFullScreen="allowfullscreen" height="400" style={{ width: "90%" }} src={props.video}></iframe>
+            <iframe
+              width="560"
+              height="315"
+              src={trailer}
+              title={props.nome}
+            ></iframe>
           </Row>
           <Row className="mt-5 d-flex align-items-center justify-content-center">
-          <p className="textPlayVideo">{props.text}</p>
+            <p className="textPlayVideo">{props.text}</p>
           </Row>
         </Modal.Body>
       </Modal>
--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -2,40 +2,42 @@ import React from "react";
 import NavbarHome from "../components/NavbarHome";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { useState, useEffect } from "react";
-import { topRated } from "../scripts/movie";
+import { topRated, trendingWeek } from "../scripts/movie";
+import { trendingWeekTV } from "../scripts/tv";
 import SliderHome from "../components/SliderHome";
 import MyFooter from "../components/MyFooter";
 import axios from "axios";
 import { MDBIcon } from "mdb-react-ui-kit";
 import GuardaFilm2 from "../components/GuardaFilm2";
 
-
 const Home = () => {
   let [filmsTop, setFilms] = useState([]);
   let [filmsTrending, setTrending] = useState([]);
   let [trendingTv, setTrendingTv] = useState([]);
 
-  async function asyncCall() {
+  async function asyncCallTopRated() {
     console.log("calling");
     const result = await topRated();
     setFilms(result);
+    console.log(result);
+  }
+  async function asyncCallTrendingWeek() {
+    console.log("calling");
+    const result = await trendingWeek();
+    setTrending(result);
+    console.log(result);
+  }
+  async function asyncCallTrendingTV() {
+    console.log("calling");
+    const result = await trendingWeekTV();
+    setTrendingTv(result);
+    console.log(result);
   }
 
   useEffect(() => {
-    asyncCall();
-  }, []);
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:2000/api/movie/trendingWeek`)
-      .then(function (response) {
-        setTrending(response.data);
-      });
-    axios
-      .get(`http://localhost:2000/api/tv/trendingWeek`)
-      .then(function (response) {
-        setTrendingTv(response.data);
-      });
+    asyncCallTopRated();
+    asyncCallTrendingTV();
+    asyncCallTrendingWeek();
   }, []);
 
   const scrollRight = () => {
@@ -63,9 +65,8 @@ const Home = () => {
         fluid
         className="overflow-auto horizontalrow d-flex align-items-center justify-content-center"
       >
-        
         <MDBIcon
-          style={{ zIndex: "1"}}
+          style={{ zIndex: "1" }}
           size="3x"
           id="slide"
           type="button"
@@ -73,11 +74,19 @@ const Home = () => {
           fas
           icon="angle-left"
         />
-        <Row className="flex-nowrap scorri d-flex align-items-center" style={{height: '330px'}} id="riga1">
+        <Row
+          className="flex-nowrap scorri d-flex align-items-center"
+          style={{ height: "330px" }}
+          id="riga1"
+        >
           {filmsTop != undefined ? (
             filmsTrending.map((element) => (
               <Col id="content" key={element.id}>
-                <GuardaFilm2 img={element.img} nome={element.title}/>
+                <GuardaFilm2
+                  img={element.img}
+                  nome={element.title}
+                  id={element.id}
+                />
               </Col>
             ))
           ) : (
@@ -91,7 +100,7 @@ const Home = () => {
           onClick={scrollRight}
           fas
           icon="angle-right"
-          style={{ zIndex: "1"}}
+          style={{ zIndex: "1" }}
         />
       </Container>
 
@@ -101,7 +110,7 @@ const Home = () => {
         className="overflow-auto mb-2 horizontalrow d-flex align-items-center justify-content-center"
       >
         <MDBIcon
-          style={{ zIndex: "1"}}
+          style={{ zIndex: "1" }}
           size="4x"
           id="slide"
           type="button"
@@ -111,9 +120,12 @@ const Home = () => {
         />
         <Row className="flex-nowrap mt-2 mb-2 scorri" id="riga2">
           {trendingTv.map((element) => (
-            <Col key={element.id}>
-              <GuardaFilm2 img={element.img} nome={element.title}/>
-              <p className="text-center mt-2 text-title">{element.title}</p>
+            <Col id="content" key={element.id}>
+              <GuardaFilm2
+                img={element.img}
+                nome={element.title}
+                id={element.id}
+              />
             </Col>
           ))}
         </Row>
@@ -124,7 +136,7 @@ const Home = () => {
           onClick={scrollRight1}
           fas
           icon="angle-right"
-          style={{ zIndex: "1"}}
+          style={{ zIndex: "1" }}
         />
       </Container>
 
