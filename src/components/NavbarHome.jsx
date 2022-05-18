3c892f1f95dc28516c172b362642fdd6786c38de
Alberto02001 <albertosoldiviero@email.com>
2022-05-19 00:10:49 +0200

    sistemati colori navbar

--- src/components/NavbarHome.jsx
+++ src/components/NavbarHome.jsx
@@ -32,22 +32,21 @@ const NavbarHome = () => {
           >
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-xl`}>
-                Offcanvas
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="/home">Home</Nav.Link>
-                <Nav.Link href="/serieTV">Serie TV</Nav.Link>
-                <Nav.Link href="/film">Film</Nav.Link>
-                <Nav.Link href="/preferiti">Preferiti</Nav.Link>
-                <Nav.Link href="/notifiche">
+                <Nav.Link className="linkNavBar" href="/home">Home</Nav.Link>
+                <Nav.Link className="linkNavBar" href="/serieTV">Serie TV</Nav.Link>
+                <Nav.Link className="linkNavBar" href="/film">Film</Nav.Link>
+                <Nav.Link className="linkNavBar" href="/preferiti">Preferiti</Nav.Link>
+                <Nav.Link className="linkNavBar" href="/notifiche">
                   <MDBIcon far icon="bell" size="lg" />
                 </Nav.Link>
-                <Nav.Link href="/ricerca">
+                <Nav.Link className="linkNavBar" href="/ricerca">
                   <MDBIcon fas icon="search" size="lg" />
                 </Nav.Link>
-                <Nav.Link href="/">
+                <Nav.Link className="linkNavBar" href="/">
                   <MDBIcon fas icon="sign-out-alt" size="lg" />
                 </Nav.Link>
               </Nav>
